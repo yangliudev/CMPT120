45fa495be07eebd13144266ebe43ecc548a2c32f
Yang Liu <ylyangliu1@gmail.com>
2022-01-28 09:50:35 -0800

    added code

--- chatbot-week3.py
+++ chatbot-week3.py
@@ -1,20 +1,49 @@
 # Yang Liu
 # CMPT 120 D400
-# January 26, 2022
+# January 28, 2022
 # This program is a chatbot
 
-def yangChatbot():
+def music_chatbot():
     
-    # Ask for the user's name
-    user_name = input("Welcome to Yang's chatbot v2! Nice to meet you, may I ask for your name?")
+    # QUESTION 1 - Ask for the user's name
+    user_name = input("Welcome to Yang's music chatbot! Nice to meet you, may I ask for your name? ")
     print('Hello ' + user_name + '!')
 
-    # Define list of colors
-    colors = ['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'black', 'white']
+    # Define list of music genres
+    genres = ['pop', 'hip-hop', 'rap', 'rock', 'edm', 'metal', 'k-pop', 'classical', 'country', 'rnb']
 
-    # QUESTION 1 - Get user's favorite color
-    fav_color = input('What is your favorite color?').lower().strip("~!@#$%^&*()_+,./;'[]")
+    # QUESTION 2 - Get user's favorite genre
+    fav_genre = input('What is your favorite genre of music? ').lower().strip("~!@#$%^&*()_+,./;'[]")
 
-    print(fav_color)
+    if fav_genre in genres:
+        print('Your favorite genre is amongst the popular ones!')
+    else:
+        print("Nice, you have a unique taste and have different preferences compared to the majority.")
 
-yangChatbot()
+    # Define list of instruments
+    instruments = ['piano', 'violin', 'trumpet', 'clarinet', 'flute', 'saxophone', 'guitar']
+
+    # QUESTION 3 - Get user's favorite instrument
+    fav_instrument = input('What is your favorite instrument? ').lower().strip("~!@#$%^&*()_+,./;'[]")
+
+    if fav_instrument in instruments:
+        if fav_genre in genres:
+            print('Your favorite instrument and genre are amongst the most popular ones!')
+        else:
+            print('Your favorite instrument is amongst the most popular ones!')
+    else:
+        print('Nice, you have a unique taste and have different preferences compared to the majority.')
+
+    replies = [user_name, fav_genre, fav_instrument]
+
+    print("It was fun getting to know you! I will now list out the things I've learned about you")
+
+    for i in range(len(replies)):
+        if i == 0:
+            print('Your name is', replies[0])
+        elif i == 1:
+            print('Your favorite genre of music is', replies[1])
+        elif i == 2:
+            print('Your favorite instrument is', replies[2])
+        
+music_chatbot()
