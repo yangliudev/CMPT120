a42470a99b671a96f8051021b7076a29bb5c7ef0
Yang Liu <ylyangliu1@gmail.com>
2022-01-27 16:20:50 -0800

    -

--- chatbot-week3.py
+++ chatbot-week3.py
@@ -5,9 +5,16 @@
 
 def yangChatbot():
     
-    #Ask for the user's name
-    user_name = input('Hi, nice to meet you. What is your name?')
+    # Ask for the user's name
+    user_name = input("Welcome to Yang's chatbot v2! Nice to meet you, may I ask for your name?")
+    print('Hello ' + user_name + '!')
 
-    print(user_name)#test
+    # Define list of colors
+    colors = ['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'black', 'white']
+
+    # QUESTION 1 - Get user's favorite color
+    fav_color = input('What is your favorite color?').lower().strip("~!@#$%^&*()_+,./;'[]")
+
+    print(fav_color)
 
 yangChatbot()
