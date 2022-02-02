e093e6497810b1b2131efac4ea0cbeb63095bd31
Yang Liu <ylyangliu1@gmail.com>
2022-02-02 10:00:13 -0800

    -

--- week4-proteins-yang.py
+++ week4-proteins-yang.py
@@ -35,7 +35,7 @@ def protein():
         else:
             amino_acid_count_dictionary['error'] += 1
 
-    print(amino_acid_count_dictionary)
+    #print(amino_acid_count_dictionary)
 
     # Get first amino acid letter
     if amino_acid_sequence[0] in amino_acid_reference_dictionary:
