28b45229ab745df7592e641cebc8402e690681b6
yangliudev <ylyangliu1@gmail.com>
2022-03-16 17:13:26 -0700

    cleanup

--- week-9/main.py
+++ week-9/main.py
@@ -1,17 +1,12 @@
 import cmpt120image
 import cmpt120imageManip
-
 import pathlib
-import subprocess
 
 def main():
     img = cmpt120image.getImage(str(pathlib.Path(__file__).parent.resolve()) + '/bird.png')
 
-    # cmpt120imageManip.blackWhite(img)
-    # cmpt120imageManip.swapRedGreen(img)
+    cmpt120imageManip.blackWhite(img)
+    cmpt120imageManip.swapRedGreen(img)
     cmpt120imageManip.reflect(img)
-
-    # Copy output to clipboard
-    # subprocess.run("pbcopy", universal_newlines=True, input=str(img))
     
 main()
